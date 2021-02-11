7b1b23143b99a7b1cd8dac5b103833c7b6e1c301
shalithacode <shalithalakruwan1998@gmail.com>
2021-02-12 00:00:18 +0530

    background changer with hex type colors

--- hex.js
+++ hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const hexColorBtn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+hexColorBtn.addEventListener("click", function(){
+
+    let hexString = '#';
+    for(let i = 0; i<6; i++){
+        hexString += hex[getRandomNumber()];
+    }
+    color.textContent = hexString;
+    document.body.style.backgroundColor = hexString;
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);;
+}
